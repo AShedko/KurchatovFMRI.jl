d02b30a8a0a03bbf47119b2dfaf56fdb99c8ac46
AShedko <ayshedko@yandex.ru>
2017-12-11 22:09:01 +0300

    Initial commit, just the type

--- src/KurchatovFMRI.jl
+++ src/KurchatovFMRI.jl
@@ -1,5 +1,26 @@
 module KurchatovFMRI
 
+export all
+using NIfTI
+
 # package code goes here
+
+abstract type AbstractKData end
+
+type KData{T} <: AbstractKData
+    root_path    :: String;
+    subject      :: Int16;
+    pattern_data :: Regex;
+    data         :: Union{NIfTI.NIVolume, Vector{NIfTI.NIVolume}}
+    pattern_meta :: Regex;
+    meta         :: T
+end
+
+function KData(conf_str)
+    valid_config(conf_str) | error("Not a valid config")
+    print("Hi")
+end
+
+valid_config(s) = length(s)
 
 end # module
